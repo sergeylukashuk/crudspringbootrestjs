4a53d4819ca4769c5cff3d503aab5539ed54d189
sergeylukashuk <sergeylukashuk@yandex.ru>
2021-01-04 03:44:58 +0500

    1.1

--- src/main/java/com/example/crudspringbootrestjs/controller/UsersController.java
+++ src/main/java/com/example/crudspringbootrestjs/controller/UsersController.java
@@ -1,73 +1,16 @@
 package com.example.crudspringbootrestjs.controller;
 
-import com.example.crudspringbootrestjs.model.Role;
-import com.example.crudspringbootrestjs.model.User;
-import com.example.crudspringbootrestjs.service.RoleService;
-import com.example.crudspringbootrestjs.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.Set;
-
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RoleService roleService;
-
     @GetMapping
     public String printUserInfo(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        model.addAttribute("user", userService.findUserByName(auth.getName()));
         return "userpage";
     }
-
-    /*@GetMapping("/{id}")
-    public String getUser(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userService.getUser(id));
-        return "users/userpage";
-    }
-
-    @PostMapping("/{id}")
-    public String updateUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @PathVariable("id") long id) {
-        if (bindingResult.hasErrors()) {
-            return "admin";
-        }
-        userService.updateUser(id, user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/index")
-    public String allUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "users/index";
-    }
-
-    @PostMapping("/new")
-    public String createUser(@RequestParam(value = "name") String name,
-                             @RequestParam(value = "last_name") String last_name,
-                             @RequestParam(value = "age") Integer age,
-                             @RequestParam(value = "email") String email,
-                             @RequestParam(value = "password") String password,
-                             @RequestParam(value = "roles") Long[] role) {
-        Set<Role> roleSet = new HashSet<>();
-        for (Long roles : role) {
-            roleSet.add(roleService.findRoleById(roles));
-        }
-        userService.addUser(new User(name, last_name, age, email, password, roleSet));
-        return "redirect:/admin";
-    }*/
 }
 
---
+++ src/main/resources/static/js/2.js
@@ -0,0 +1,32 @@
+$(document).ready(async function () {
+
+        let url = '/rest' + location.href.substring(location.href.indexOf('/admin'), location.href.length)
+        let userId = $("#tableBody").data('id')
+        if (url.endsWith("users") == true) url = '/rest/admin/' + userId
+        let users = await fetch(url).then(response => response.json())
+
+        let listRoles = '';
+        for (let element of users.authorities) {
+            listRoles += " " + element.role;
+        }
+        let userid = users.id;
+        let username = users.name;
+        let userlastname = users.last_name;
+        let userage = users.age;
+        let useremail = users.email;
+        let userroles = listRoles;
+
+        let tr = $("<tr>");
+        tr.append("" +
+            "<td>" + userid + "</td>" +
+            "<td>" + username + "</td>" +
+            "<td>" + userlastname + "</td>" +
+            "<td>" + userage + "</td>" +
+            "<td>" + useremail + "</td>" +
+            "<td>" + userroles + "</td>");
+        $("#myTable").append(tr)
+});
+
+
+
+
